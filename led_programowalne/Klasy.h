e963e1fb36b4386702d433dc05bdecf19c355677
v33r005 <125038833+v33r005@users.noreply.github.com>
2026-04-12 15:59:31 +0200

    Update Klasy.h

--- led_programowalne/Klasy.h
+++ led_programowalne/Klasy.h
@@ -3,32 +3,32 @@
 #include <supla/control/rgb_base.h>
 #include <supla/control/dimmer_base.h>
 
-class RgbLeds : public Supla::Control::RGBBase {
+class RgbLeds : public Supla::Control::RGBWBase {
   public:
     RgbLeds(int Channel)
       : Channel(Channel) {
     }
     void setRGBWValueOnDevice( uint32_t  red,
                                uint32_t  green,
-                               uint32_t  blue,
-                               uint32_t  colorBrightness,
-                               uint32_t  brightness) {
+                               uint32_t  blue//,
+                               //uint32_t  brightness) 
+                               ){
 
 
         
         redWSLED2             = red;
         greenWSLED2           = green;
         blueWSLED2            = blue;
-        redWSLED1             = map(red,             0, 1023, 0, 255);
-        greenWSLED1           = map(green,           0, 1023, 0, 255);
-        blueWSLED1            = map(blue,            0, 1023, 0, 255);
-        colorBrightnessWSLED1 = map(colorBrightness, 0, 1023, 0, 100);
-        jasnosc = colorBrightnessWSLED1;
+        redWSLED1             = red;//map(red,             0, 1023, 0, 255);
+        greenWSLED1           = green;//map(green,           0, 1023, 0, 255);
+        blueWSLED1            = blue;//map(blue,            0, 1023, 0, 255);
+       // colorBrightnessWSLED1 = map(brightness, 0, 1023, 0, 100);
+       // jasnosc = colorBrightnessWSLED1;
 
        
-        redWSLED             = (redWSLED1 * colorBrightnessWSLED1 ) / 100;
-        greenWSLED           = (greenWSLED1 * colorBrightnessWSLED1) / 100;
-        blueWSLED            = (blueWSLED1 * colorBrightnessWSLED1) / 100;
+        redWSLED             = (redWSLED1);// * colorBrightnessWSLED1 ) / 100;
+        greenWSLED           = (greenWSLED1);// * colorBrightnessWSLED1) / 100;
+        blueWSLED            = (blueWSLED1);// * colorBrightnessWSLED1) / 100;
 
     }
   protected:
@@ -42,7 +42,7 @@ class RgbLeds : public Supla::Control::RGBBase {
 
 RgbLeds *ledyProgramowalne = {};
 
-class Dimm : public Supla::Control::DimmerBase {
+class Dimm : public Supla::Control::DimmerLeds {
   public:
     Dimm(int Channel)
       : Channel(Channel) {
@@ -50,10 +50,10 @@ class Dimm : public Supla::Control::DimmerBase {
     void setRGBWValueOnDevice(uint32_t red,
                               uint32_t green,
                               uint32_t blue,
-                              uint32_t colorBrightness,
+                              //uint32_t colorBrightness,
                               uint32_t brightness) {
 
-        regulacja = map(brightness, 0, 1023, 0, 255);
+        regulacja = brightness;//map(brightness, 0, 1023, 0, 255);
     }
   protected:
     int Channel;
