b0016b755564e02a14bbed61273bf799bd902c96
v33r005 <125038833+v33r005@users.noreply.github.com>
2026-05-18 08:04:41 +0200

    Update RGB Class

--- led_programowalne/Klasy.h
+++ led_programowalne/Klasy.h
@@ -3,46 +3,28 @@
 #include <supla/control/rgb_base.h>
 #include <supla/control/dimmer_base.h>
 
-class RgbLeds : public Supla::Control::RGBWBase {
+class RgbLeds : public Supla::Control::RGBBase {
   public:
     RgbLeds(int Channel)
       : Channel(Channel) {
     }
     void setRGBWValueOnDevice( uint32_t  red,
                                uint32_t  green,
-                               uint32_t  blue//,
-                               //uint32_t  brightness) 
-                               ){
-
-
+                               uint32_t  blue,
+                               uint32_t  brightness) {
         
-        redWSLED2             = red;
-        greenWSLED2           = green;
-        blueWSLED2            = blue;
-        redWSLED1             = red;//map(red,             0, 1023, 0, 255);
-        greenWSLED1           = green;//map(green,           0, 1023, 0, 255);
-        blueWSLED1            = blue;//map(blue,            0, 1023, 0, 255);
-       // colorBrightnessWSLED1 = map(brightness, 0, 1023, 0, 100);
-       // jasnosc = colorBrightnessWSLED1;
-
-       
-        redWSLED             = (redWSLED1);// * colorBrightnessWSLED1 ) / 100;
-        greenWSLED           = (greenWSLED1);// * colorBrightnessWSLED1) / 100;
-        blueWSLED            = (blueWSLED1);// * colorBrightnessWSLED1) / 100;
+        redWSLED             = red;
+        greenWSLED           = green;
+        blueWSLED            = blue;
 
     }
   protected:
-    uint32_t redWSLED1;
-    uint32_t greenWSLED1;
-    uint32_t blueWSLED1;
-    uint32_t colorBrightnessWSLED1;
-    uint32_t brightnessWSLED;
     int Channel;
 };
 
-RgbLeds *ledyProgramowalne = {};
+RgbLeds *ledyProgramowalne = nullptr;
 
-class Dimm : public Supla::Control::DimmerLeds {
+class Dimm : public Supla::Control::DimmerBase {
   public:
     Dimm(int Channel)
       : Channel(Channel) {
@@ -52,7 +34,7 @@ class Dimm : public Supla::Control::DimmerLeds {
                               uint32_t blue,
                               uint32_t brightness) {
 
-        regulacja = brightness;//map(brightness, 0, 1023, 0, 255);
+        regulacja = map(brightness, 0, 1023, 0, 255);
     }
   protected:
     int Channel;
